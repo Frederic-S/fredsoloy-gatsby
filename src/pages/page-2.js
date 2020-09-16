7dd3aa2773e8de167200b02645c94f1df9bb53de
Fred-S <fredvantage@gmail.com>
2020-09-16 16:00:18 +0200

    rm bootstrap use tailwind

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -9,6 +9,8 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the contact page</h1>
     <p>Contact page coming soon...</p>
+    <button className="text-blue-600 hover:text-red-600 ...">Button</button>
+    <br />
     <Link style={{ textDecoration: "none" }} to="/">
       homepage
     </Link>
