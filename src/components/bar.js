ec41ca40039f079d8c6207ab0ce9a39034f4d765
Fred-S <fredvantage@gmail.com>
2020-09-16 17:55:35 +0200

    added progress bar

---
+++ src/components/bar.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+const ProgressBar = props => {
+  const { bgcolor, completed } = props
+
+  const containerStyles = {
+    height: 20,
+    width: "100%",
+    backgroundColor: "#e6acfc",
+    borderRadius: 50,
+    margin: 50,
+  }
+
+  const fillerStyles = {
+    height: "100%",
+    width: `${completed}%`,
+    backgroundColor: bgcolor,
+    borderRadius: "inherit",
+    textAlign: "right",
+  }
+
+  const labelStyles = {
+    padding: 15,
+    color: "white",
+    fontWeight: "bold",
+  }
+
+  return (
+    <div style={containerStyles}>
+      <div style={fillerStyles}>
+        <span style={labelStyles}>{`${completed}%`}</span>
+      </div>
+    </div>
+  )
+}
+
+export default ProgressBar
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,23 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import ProgressBar from "../components/bar"
+
+const testData = [{ bgcolor: "#6a1b9a", completed: 25 }]
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to my new personal site.</p>
-    <p>Now I'm building it, ready soon.</p>
+    <p>Ready soon, progress...</p>
+    {testData.map((item, idx) => (
+      <ProgressBar
+        key={idx}
+        bgcolor={item.bgcolor}
+        completed={item.completed}
+      />
+    ))}
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
